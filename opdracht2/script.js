80c41c2190fb56e05667a8b7a1719084c5985bec
Jorien <jorienkorn@hotmail.com>
2019-03-12 13:56:05 +0100

    v2

--- opdracht2/script.js
+++ opdracht2/script.js
@@ -1,31 +1,74 @@
+
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
 /*eslint-env browser*/
+
 /*eslint 'no-console': 0*/
 
-var left = document.querySelector('.leftbutton');
-var right = document.querySelector('.rightbutton');
-var buttons = document.querySelector('.button');
+var buttonleft = document.querySelector('.leftbutton');
+var buttonright = document.querySelector('.rightbutton');
+var buttons = document.querySelectorAll('.button');
+var div = document.querySelector('div');
+var currentImage = 1;
 
-var i;
-
-function check() {
-    var className = this.classList[1];
-    
-    if (className === 'leftbutton') {
-        console.log('left');
-    } else if (className === 'rightbutton') {
-        console.log('right');
+function animateLeft() {
+    currentImage--;
+    console.log('left', currentImage);
+    div.classList.add('moveleft');
+    if (currentImage = 2) {
+        div.classList.add('moveleft2');
+    }
+    if (currentImage = 3) {
+        div.classList.add('moveleft3');
+    }
+    if (currentImage = 4) {
+        div.classList.add('moveleft4');
+    }
+    if (currentImage = 5) {
+        div.classList.add('moveleft5');
     }
 }
 
-for(i=0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', check);
+function animateRight() {
+    currentImage++;
+    console.log('right');
+    div.classList.add('moveright');
+    if (currentImage = 2) {
+        div.classList.add('moveright2');
+    }
+    if (currentImage = 3) {
+        div.classList.add('moveright3');
+    }
+    if (currentImage = 4) {
+        div.classList.add('moveright4');
+    }
+    if (currentImage = 5) {
+        div.classList.add('moveright5');
+    }
 }
 
-//function goRight(e) {
-//    console.log(this);
+buttonleft.addEventListener('click', animateLeft);
+buttonright.addEventListener('click', animateRight);
+
+
+
+
+//var i;
+
+
+//function check() {
+//    var className = this.classList[1];
 //    
+//    if(className === "leftbutton"){
+//        console.log('left');
+//        div.classList.add='.moveleft';
+//    }
+//    else if(className === "rightbutton"){
+//        console.log('right');
+//        div.classList.add='.moveright';
+//    }
 //}
-//
-//arrowLeft.addEventListener("click", goLeft);
-//arrowRight.addEventListener("click", goRight);
+
+//for(i = 0; i < buttons.length; i++){
+//    buttons[i].addEventListener('click', check);
+//}
